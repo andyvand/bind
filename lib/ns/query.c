5a29c7ae78d566dfbe1d0d157e72250aee0f371d
Evan Hunt <each@isc.org>
2018-11-01 08:53:49 -0700

    continue with implementation of dns64 as an external module
    - namedconf.c now has dns64-related options flagged as obsolete.
    - lib/dns/dns64.c and lib/dns/include/dns/dns64.h are gone. their
      code has been migrated into bin/plugins/dns64.c, with corresponding
      nomenclature changes, and some style cleanup.
    - dns64-related fields have been removed from the dns_view struct.
    - dns64 configuration parsing code has been moved from named into
      the plugin module.
    - checkconf tests have been removed from the dns64 system test
      these will be restored later, when named-checkconf has been
      modified to be able to load plugin modules and check their
      configuration syntax.
    - dns64-related configuration checks have been removed from
      lib/bind9/check.c.
    - zonemgr is now attached to the view so that modules will be able
      to access it.

--- lib/ns/query.c
+++ lib/ns/query.c
@@ -32,7 +32,6 @@
 #include <dns/cache.h>
 #include <dns/db.h>
 #include <dns/dlz.h>
-#include <dns/dns64.h>
 #include <dns/dnsrps.h>
 #include <dns/dnssec.h>
 #include <dns/events.h>
@@ -8491,12 +8490,6 @@ query_coveringnsec(query_ctx_t *qctx) {
 		if (qctx->type == dns_rdatatype_any) {	/* XXX not yet */
 			goto cleanup;
 		}
-		if (!ISC_LIST_EMPTY(qctx->view->dns64) &&
-		    (qctx->type == dns_rdatatype_a ||
-		     qctx->type == dns_rdatatype_aaaa)) /* XXX not yet */
-		{
-			goto cleanup;
-		}
 		if (!qctx->resuming && !STALE(qctx->rdataset) &&
 		    qctx->rdataset->ttl == 0 && RECURSIONOK(qctx->client))
 		{
@@ -8553,12 +8546,6 @@ query_coveringnsec(query_ctx_t *qctx) {
 	switch (result) {
 	case ISC_R_SUCCESS:
 		if (qctx->type == dns_rdatatype_any) {	/* XXX not yet */
-			goto cleanup;
-		}
-		if (!ISC_LIST_EMPTY(qctx->view->dns64) &&
-		    (qctx->type == dns_rdatatype_a ||
-		     qctx->type == dns_rdatatype_aaaa)) /* XXX not yet */
-		{
 			goto cleanup;
 		}
 		/* FALLTHROUGH */
